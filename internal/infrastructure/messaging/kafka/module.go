1b4af03747d8781f8d6dcc408c49331d3975f146
Sokol111 <igorsokol111@gmail.com>
2025-11-16 13:20:37 +0100

    u

--- internal/infrastructure/messaging/kafka/module.go
+++ internal/infrastructure/messaging/kafka/module.go
@@ -1,24 +1,41 @@
 package kafka
 
 import (
+	"context"
 	"reflect"
 
 	"github.com/Sokol111/ecommerce-category-service-api/events"
+	"github.com/Sokol111/ecommerce-commons/pkg/messaging/kafka/config"
 	"github.com/Sokol111/ecommerce-commons/pkg/messaging/kafka/consumer"
 	"go.uber.org/fx"
+	"go.uber.org/zap"
 )
 
 func Module() fx.Option {
-	typeMapping := consumer.TypeMapping{
-		events.EventTypeCategoryCreated: reflect.TypeOf(&events.CategoryCreatedEvent{}),
-		events.EventTypeCategoryUpdated: reflect.TypeOf(&events.CategoryUpdatedEvent{}),
+	return fx.Options(
+		fx.Provide(provideDeserializer),
+		consumer.RegisterHandlerAndConsumer("category-events", newCategoryHandler),
+	)
+}
+
+func provideDeserializer(lc fx.Lifecycle, kafkaConf config.Config, log *zap.Logger) (consumer.Deserializer, error) {
+	// Map Avro schema full names to Go types
+	typeMap := consumer.TypeMapping{
+		"com.ecommerce.events.category.CategoryCreatedEvent": reflect.TypeOf(events.CategoryCreatedEvent{}),
+		"com.ecommerce.events.category.CategoryUpdatedEvent": reflect.TypeOf(events.CategoryUpdatedEvent{}),
 	}
 
-	return fx.Provide(
-		consumer.RegisterHandlerAndConsumer("category-events", newCategoryHandler),
-		fx.Annotate(
-			func() consumer.TypeMapping { return typeMapping },
-			fx.ResultTags(`name:"category-events"`),
-		),
-	)
+	deserializer, err := consumer.NewAvroDeserializer(kafkaConf.SchemaRegistry, typeMap)
+	if err != nil {
+		return nil, err
+	}
+
+	lc.Append(fx.Hook{
+		OnStop: func(ctx context.Context) error {
+			log.Info("closing schema registry deserializer")
+			return deserializer.Close()
+		},
+	})
+
+	return deserializer, nil
 }

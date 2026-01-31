bd551fcf7f73d01091d5f9f821710a4f3dbd5ee5
Sokol111 <igorsokol111@gmail.com>
2026-01-31 19:50:30 +0100

    u

--- internal/infrastructure/persistence/mongo/category_view_repository.go
+++ internal/infrastructure/persistence/mongo/category_view_repository.go
@@ -6,26 +6,23 @@ import (
 	"github.com/Sokol111/ecommerce-category-query-service/internal/domain/categoryview"
 	"github.com/Sokol111/ecommerce-commons/pkg/core/logger"
 	commonsmongo "github.com/Sokol111/ecommerce-commons/pkg/persistence/mongo"
-	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.uber.org/zap"
 )
 
 type categoryViewRepository struct {
 	*commonsmongo.GenericRepository[categoryview.CategoryView, categoryViewEntity]
-	collection commonsmongo.Collection
-	mapper     *categoryViewMapper
+	mapper *categoryViewMapper
 }
 
 func newCategoryViewRepository(mongo commonsmongo.Mongo, mapper *categoryViewMapper) (categoryview.Repository, error) {
-	collection := mongo.GetCollection("category_view")
-	genericRepo, err := commonsmongo.NewGenericRepository(collection, mapper)
+	genericRepo, err := commonsmongo.NewGenericRepository(mongo.GetCollection("category_view"), mapper)
 	if err != nil {
 		return nil, err
 	}
 
 	return &categoryViewRepository{
 		GenericRepository: genericRepo,
-		collection:        collection,
 		mapper:            mapper,
 	}, nil
 }
